9183295b252c050911fc0ae3749104c1c72d6771
Brian Barrow <briancbarrow@gmail.com>
2019-01-27 20:08:33 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Header } from './components/common/index'
+
+const App = () => {
+  return (
+    <View>
+      <Header headerText="TDEE Calculator" />
+    </View>
+  )
+}
+
+export default App
